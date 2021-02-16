2200a801c749f8f24cf581fa8e795e69138138db
TheDailyWaffle <32621775+TheDailyWaffle@users.noreply.github.com>
2021-02-16 09:40:54 -0700

    Delete Chapter8.py

--- Y2V3/Chapter8.py
+++
@@ -1,6 +0,0 @@
-import requests
-response = requests.get('https://youkosoapi.rin.one/getPaste?id=Xdw6AWE0')
-print(response.text)
-file = open("output.txt", "w")
-file.write(response.text)
-file.close()
